6da1d7d8df1176cb18d6a73ce99eee4300ef3994
Ishan Sethi <ishansethi8@gmail.com>
2018-05-16 08:24:43 -0400

    Raw Model Loading Added

--- src/engine_tester/MainLoop.java
+++ src/engine_tester/MainLoop.java
@@ -1,20 +1,55 @@
 package engine_tester;
 
 import org.lwjgl.opengl.Display;
-import render_engine.DisplayManager;
 
+import render_engine.DisplayManager;
+import render_engine.Loader;
+import render_engine.RawModel;
+import render_engine.Renderer;
+
+/**
+ * This class contains the main method and is used to test the engine.
+ *
+ * @author Karl
+ *
+ */
 public class MainLoop {
 
-    public static void main(String[] args){
+
+    /**
+     * Loads up the position data for two triangles (which together make a quad)
+     * into a VAO. This VAO is then rendered to the screen every frame.
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
 
         DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
 
-        while(!Display.isCloseRequested()){
+        float[] vertices = {
+                // Left bottom triangle
+                -0.5f, 0.5f, 0f,
+                -0.5f, -0.5f, 0f,
+                0.5f, -0.5f, 0f,
+                // Right top triangle
+                0.5f, -0.5f, 0f,
+                0.5f, 0.5f, 0f,
+                -0.5f, 0.5f, 0f
+        };
 
+        RawModel model = loader.loadToVAO(vertices);
 
-            //App logic
-
+        while (!Display.isCloseRequested()) {
+            // game logic
+            renderer.prepare();
+            renderer.render(model);
             DisplayManager.updateDisplay();
         }
+
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
     }
+
 }
